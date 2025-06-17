0b516d2723db55f801c0592b2bcd2858843fb9e5
Ntseno Ntseno <ntseno.ntseno@gmail.com>
2025-06-17 08:58:21 +0200

    changes to the extract.py scrip

--- scripts/extract.py
+++ scripts/extract.py
@@ -15,7 +15,7 @@ def extract_data(url: str):
         response = requests.get(url)
         return response.json()
     except:
-        error_message = f'Error: Something went wrong while extracting data with your api key @{datetime.datetime.now()}\n'
+        error_message = f'Error {datetime.datetime.now()}: Something went wrong while extracting data with your api key\n'
         log(error_message)
 
 
@@ -27,11 +27,11 @@ if raw_data is None:
     print('An Error has occured, check the program.log file for more details')
 
 elif isinstance(raw_data, list) or isinstance(raw_data, dict): # Public AIP call 
-    log_message = f'Message: Data has been extracted successfuly from a public API on an API call @{datetime.datetime.now()}\n'
+    log_message = f'Message {datetime.datetime.now()}: Data has been extracted successfuly from a public API on an API call\n'
     log(log_message)
     with open('data/raw/raw_data.json', 'w', encoding='utf-8') as f:
         json.dump(raw_data, f, ensure_ascii=False, indent=4)
-    log_message = f'Message: Data has been successfuly saved on the raw directory of the project @{datetime.datetime.now()}\n'
+    log_message = f'Message {datetime.datetime.now()}: Data has been successfuly saved on the raw directory of the project\n'
     log(log_message)
 
 
